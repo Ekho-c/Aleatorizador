9a42dcd526651e409d37cbe7be0edaa6f5b1e8b4
Gabriel <gsouzagoncalves2024@gmail.com>
2024-10-22 00:01:16 -0300

    Adição de um indicador da cor atual, melhoria no CSS e atualização no JS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const botaoCor = document.getElementById('botaoCor');
 const botaoSec = document.getElementById('botaoSec');
+const colorText = document.getElementById('colorText');
 
 // Função para gerar uma cor
 
@@ -12,9 +13,11 @@ function getRandomColor() {
     return color;
 }
 
-// Função para trocar a cor de fundo
+// Função para trocar a cor de fundo e atualizar o texto
 botaoCor.addEventListener('click', function() {
-    document.body.style.backgroundColor = getRandomColor();
+    const newColor = getRandomColor();
+    document.body.style.backgroundColor = newColor;
+    colorText.textContent = `Cor atual: ${newColor}`;
 });
 
 //função para a imagem
